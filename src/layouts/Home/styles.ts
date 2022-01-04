4b0ae1d8cf799a69678a86bf3b0b316e4003a4a0
benitezhiago <hiagosantanabenitez@gmail.com>
2022-01-04 11:16:28 -0400

    first commit

---
+++ src/layouts/Home/styles.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const Container = styled.section`
+  width: 100%;
+
+  img {
+    width: 100%;
+  }
+`;
---
+++ src/styles/global.ts
@@ -0,0 +1,26 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyles = createGlobalStyle`
+
+    * {
+      margin: 0;
+      padding: 0;
+      box-sizing: border-box;
+      -webkit-font-smoothing: antialiased;
+      -moz-osx-font-smoothing: grayscale;
+  
+    }
+  body {
+    overflow-x: hidden;
+  }
+  ::-webkit-scrollbar {
+  background: #020005;
+  width: 12px;
+}
+::-webkit-scrollbar-thumb {
+  background: linear-gradient(180deg, rgba(102, 54, 221, 0) 0%, #6636DD 100%);
+  border-radius: 6px;
+}
+  `;
+
+export default GlobalStyles;
